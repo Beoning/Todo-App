aab1724386d90456c390542ff7aec9dc00a72efa
Даниил Никитин <faverus061999@gmail.com>
2021-03-08 13:59:54 +0300

    Custom hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
+import React, { memo } from "react";
+import useInputValue from "./Custom-hooks/useInputValue";
+import useTodos from "./Custom-hooks/useTodos";
+import Layout from "./Components/Layout";
+import AddTodo from "./Components/AddTodo";
+import TodoList from "./Components/TodoList";
+const TodoApp = memo((props) => {
+  const { inputValue, changeInput, clearInput, keyInput } = useInputValue();
+  const { todos, addTodo, checkTodo, removeTodo } = useTodos();
+  const clearInputAndAddTodo = (_) => {
+    clearInput();
+    addTodo(inputValue);
+  };
+  return (
+    <Layout>
+      <AddTodo
+        inputValue={inputValue}
+        onInputChange={changeInput}
+        onButtonClick={clearInputAndAddTodo}
+        onInputKeyPress={(event) => keyInput(event, clearInputAndAddTodo)}
+      />
+      <TodoList
+        items={todos}
+        onItemCheck={(idx) => checkTodo(idx)}
+        onItemRemove={(idx) => removeTodo(idx)}
+      />
+    </Layout>
+  );
+});
 
-function App() {
-  return <div className="App"></div>;
-}
-
-export default App;
+export default TodoApp;
